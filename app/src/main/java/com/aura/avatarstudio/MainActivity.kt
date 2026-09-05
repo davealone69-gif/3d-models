4c7c26e56aa9237207bfbbf91c30a180d3943d5e
davealone69-gif <davealone69@gmail.com>
2026-09-06 01:03:09 +0800

    Initialize local Llama settings at app startup

--- app/src/main/java/com/aura/avatarstudio/MainActivity.kt
+++ app/src/main/java/com/aura/avatarstudio/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.aura.avatarstudio.api.LocalLlamaService
 import com.aura.avatarstudio.ui.AppUI
 import com.aura.avatarstudio.ui.theme.AvatarStudioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LocalLlamaService.initialize(applicationContext)
         enableEdgeToEdge()
         setContent {
             AvatarStudioTheme {
